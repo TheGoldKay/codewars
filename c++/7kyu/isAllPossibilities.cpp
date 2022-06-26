bda9551f013ef536eac3a0371b813fad5201eff0
Ruby <jhonntan.flr@gmail.com>
2022-06-25 23:29:00 -0300

    kata 1 v2

--- c++/7kyu/isAllPossibilities.cpp
+++ c++/7kyu/isAllPossibilities.cpp
@@ -1,9 +1,19 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 bool isAllPossibilities(std::vector<int> arr){
-  std::cout << "test " << arr[1] << std::endl;
-  return true;
+  int ln = arr.size();
+  if(ln <= 1){
+    if(arr[0] == 0){
+      return true;
+    }
+    return false;
+  }
+  int max = *std::max_element(arr.begin(), arr.end());
+  int min = *std::min_element(arr.begin(), arr.end());
+  std::cout << min << " " << max << std::endl;
+  return min == 0 and ln == max+1;
 }
 
 std::vector<int> v = {1, 2};
