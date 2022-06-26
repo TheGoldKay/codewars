865efb0ce6e57f1049de7509643fb4e61f74855d
Ruby <jhonntan.flr@gmail.com>
2022-06-25 23:09:33 -0300

    kata 1

---
+++ c++/7kyu/isAllPossibilities.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+
+bool isAllPossibilities(std::vector<int> arr){
+  std::cout << "test " << arr[1] << std::endl;
+  return true;
+}
+
+std::vector<int> v = {1, 2};
+
+int main(){
+  isAllPossibilities(v);
+  return 0;
+}
