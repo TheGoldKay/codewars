419dad44983b18822b5932ae91d45d01a48de0b2
ruby <jhonnatanwfc@live.com>
2022-11-01 15:45:55 -0300

    binary addiction

---
+++ python/binary_addiction.py
@@ -0,0 +1,13 @@
+def add_binary(a,b):
+    n = a + b
+    binList = []
+    while (n > 0):
+        binList.append(n % 2)
+        n = n // 2
+    return ''.join(map(str, reversed(binList)))
+
+def add_binary(a, b):
+    return bin(a+b)[2:]
+
+print(add_binary(2, 2))
+  
