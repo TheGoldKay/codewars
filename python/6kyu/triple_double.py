3fc0bd1c78f8cb940e7a0cd318d525e65c35b947
TheGoldKay <jhonnatanwfc@live.com>
2022-11-13 14:55:01 -0300

    triple double fix

--- python/6kyu/triple_double.py
+++ python/6kyu/triple_double.py
@@ -4,12 +4,14 @@ def triple_double(num1, num2):
     a = dict()
     b = dict()
     t, d = False, False 
+    num = 0
     for i in range(len(n1)-1):
         if n1[i] == n1[i+1] and n1[i+1] == n1[i+2]:
             t = True 
+            num = n1[i]
             break
     for i in range(len(n2)-1):
-        if n1[i] == n1[i+1]:
+        if n2[i] == n2[i+1] and n2[i] == num:
             d = True 
             break
         
