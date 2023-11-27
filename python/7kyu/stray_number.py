d43d1e68b9c533205625c5b94a79aad86a18b27b
TheGoldKay <jhonnatanwfc@live.com>
2023-11-27 02:07:41 -0300

    stray v2 (improved)

--- python/7kyu/stray_number.py
+++ python/7kyu/stray_number.py
@@ -2,3 +2,12 @@ def stray(arr):
     for n in arr:
         if arr.count(n) == 1:
             return n 
+
+def stray2(arr):
+    for i in range(1, len(arr), 2):
+        if arr[i] == arr[i-1] and arr[i] != arr[i+1]:
+            return arr[i+1]
+        elif arr[i] == arr[i+1] and arr[i] != arr[i-1]:
+            return arr[i-1]
+        elif  arr[i] != arr[i+1] and arr[i] != arr[i-1]:
+            return arr[i]
