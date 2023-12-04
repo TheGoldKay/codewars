b1b333e42fe44c1b879d68854268a243689c4bfa
TheGoldKay <jhonnatanwfc@live.com>
2023-12-04 19:42:24 -0300

    another spin

--- javascript/7kyu/spin_around.js
+++ javascript/7kyu/spin_around.js
@@ -9,3 +9,15 @@ function spinAround(keywords) {
   }
   return Math.floor(Math.abs(rotations) / 4);
 }
+
+function spinAround(turns) {  
+  let spins = 0;
+  for(let turn of turns){
+    if(turn === "right"){
+      spins += 0.25;
+    }else if(turn === "left"){
+      spins -= 0.25;
+    }
+  }
+  return Math.floor(Math.abs(spins));
+}
