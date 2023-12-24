23c9d73d79e079af884f800520c3d4941a04772f
jhonnatan carvalho <jhonnatanwfc@live.com>
2023-12-24 15:53:49 -0300

    hotspot

---
+++ python/7kyu/hotspot.py
@@ -0,0 +1,23 @@
+def nonstop_hotspot(area):
+    left = area.index('P') - 1
+    right = left + 2
+    hs = 0
+    while (left >= 0):
+        if(area[left] == '#'):
+            break 
+        else:
+            if(area[left] == '*'):
+                hs += 1
+            left -= 1
+    while(right < len(area)):
+        if(area[right] == '#'):
+            break 
+        else:
+            if(area[right] == '*'):
+                hs += 1
+            right += 1
+    return hs 
+
+s1 = '*   P  *   *'
+s2 = '*  * #  * P # * #'
+print(nonstop_hotspot(s2))
