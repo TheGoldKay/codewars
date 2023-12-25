45d786c58fde11cd97640ef092464d06852fb309
jhonnatan carvalho <jhonnatanwfc@live.com>
2023-12-25 08:21:26 -0300

    better version

--- python/7kyu/contact.py
+++ python/7kyu/contact.py
@@ -6,8 +6,15 @@ def contact(hallway):
         return -1
     ln = len(min(lst))
     return math.ceil(ln / 2) + (ln % 2 == 0)
+import re, math 
+
+
+def contact2(hallway):
+    pairs = re.findall('>-*<', hallway)
+    print(pairs)
+    return min(map(len, pairs)) // 2 if pairs else -1
 
 hallway = '>---------------<--------------------------<-------->---------<------->----------<----<---->>----------<------->>>---------------<<------>'
 hallway2 = '>>-----<<'
 hallway3 = '----<----->----'
-contact('><')
+print(contact2('><'))
