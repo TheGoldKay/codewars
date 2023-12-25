e1af690d96adef911943ab0678676b654f31a01e
jhonnatan carvalho <jhonnatanwfc@live.com>
2023-12-25 10:43:09 -0300

    no regex

--- python/7kyu/regex/purify.py
+++ python/7kyu/regex/purify.py
@@ -11,5 +11,33 @@ def purify(s: str) -> str:
             res.append(newh)
     return " ".join(res)
 
+def purify2(s: str) -> str:
+    s = s.replace('I', 'i')
+    s = list(s)
+    indexes = []
+    for i in range(len(s)):
+        if s[i] == 'i' and i != 0 and i != len(s) - 1:
+            if s[i-1] != ' ':
+                indexes.append(i-1)
+            indexes.append(i)
+            if s[i+1] != ' ':
+                indexes.append(i+1)
+        elif s[i] == "i" and i == 0:
+            indexes.append(0)
+            indexes.append(1)
+        elif s[i] == 'i' and i == len(s) - 1:
+            indexes.append(len(s)-1)
+            indexes.append(len(s)-2)
+    indexes = list(set(indexes))
+    indexes = sorted(indexes, reverse=True)
+
+    for i in indexes:
+        del s[i]
+    s = ''.join(s)
+    s = s.strip()
+    for i in range(10):
+        s = s.replace('  ', ' ')
+    return s
+
 print(purify("STRING"))
         
