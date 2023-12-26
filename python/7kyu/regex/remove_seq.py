d10835915940cf447aa9a12e243a47ce816c0416
jhonnatan carvalho <jhonnatanwfc@live.com>
2023-12-26 12:13:26 -0300

    remove next equal

---
+++ python/7kyu/regex/remove_seq.py
@@ -0,0 +1,13 @@
+def remove_consecutive_duplicates(s : str) -> str:
+    l = s.split(' ')
+    if(len(l) > 1):
+        res = [l[0]]
+        for i in range(1, len(l) - 1):
+            if(l[i] != l[i-1]):
+                res.append(l[i])
+        if(l[-1] != l[-2]):
+            res.append(l[-1])
+        return " ".join(res)   
+    else:
+        return l[0]
+     
