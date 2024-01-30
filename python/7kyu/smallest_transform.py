a711fc7a36890fadecde26b89f5f49152aa6d2ea
jhonnatan carvalho <jhonnatanwfc@live.com>
2024-01-30 17:22:58 -0300

    proper int

--- python/7kyu/smallest_transform.py
+++ python/7kyu/smallest_transform.py
@@ -1,8 +1,8 @@
 def smallest_transform(num):
-    s = list(str(num))
+    s = [int(s) for s in str(num)]
     least = float('inf')
     for n in s:
-        steps = sum(map(lambda x: abs(int(n) - int(x)), s))
+        steps = sum(map(lambda x: abs(n - x), s))
         if steps < least:
             least = steps
     return least
