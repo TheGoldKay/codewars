fdf6267ddb6616921548dd351607a00a167fd6ad
TheGoldKay <jhonnatanwfc@live.com>
2024-02-07 14:03:45 -0300

    neutralize

--- c/8kyu/neutralize.c
+++ c/8kyu/neutralize.c
@@ -3,27 +3,19 @@
 
 void neutralize (const char *s1, const char *s2, char *s3)
 {
-  int len = strlen(s1);
-  /* s3 = '\0'; */
-  if(len > 0)
+  int i = 0;
+  while(s1[i] != '\0')
   {
-    for(int i = 0; i < len; i++)
+    if(s1[i] == s2[i])
     {
-        if(s1[i] == s2[i])
-        {
-            printf("here\n");
-            char str[2];
-            str[0] = s1[i];
-            str[1] = '\0';
-            strcat(s3, str);
-            printf("%s\n", s3);
-        }else
-        {
-            strcat(s3, "0");
-            printf("here2\n");
-        }
+        s3[i] = s1[i];
+    }else
+    {
+        s3[i] = '0';
     }
+    i++;
   }
+  s3[i] = '\0';
 }
 
 
