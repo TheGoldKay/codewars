fa0beda1f1ccd04133788c1d87e3596af1792011
TheGoldKay <jhonnatanwfc@live.com>
2024-02-13 06:08:04 -0300

    fix

--- ruby/8kyu/funky_str.rb
+++ ruby/8kyu/funky_str.rb
@@ -1,11 +1,11 @@
 class String
-    def value 
-        self.split("").map(&:ord).sum 
+    def val
+        self.split("").count 
     end
 end
-
+ 
 def solution(a, b)
-  a.value > b.value ? "#{b}#{a}#{b}" : "#{a}#{b}#{a}"
+  a.val > b.val ? "#{b}#{a}#{b}" : "#{a}#{b}#{a}"
 end
 #a, b = ARGV
 puts solution *ARGV
