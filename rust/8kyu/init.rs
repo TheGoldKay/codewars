3a61b19db842490d5ea39584bf4354b7df3afee0
GoldKay <jhonnatanwfc@live.com>
2025-02-01 10:32:07 -0300

    rust hello

---
+++ rust/8kyu/init.rs
@@ -0,0 +1,15 @@
+// Basic entry point
+fn hello(name: String) -> () {
+    println!("Hello {}", name);
+}
+
+// OR with command line arguments
+fn main() -> () {
+    let name: String;
+    println!("Please enter your name: ");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).expect("Failed to read line");
+    input = input.trim().to_string();
+    name = input[..1].to_uppercase() + &input[1..];
+    hello(name);
+}
