4169f778f6f48e722f4e36768c7440696678f969
TheGoldKay <jhonnatanwfc@live.com>
2025-07-12 14:36:11 -0300

    better goose on the loose

--- rust/8/goose.rs
+++ rust/8/goose.rs
@@ -1,5 +1,11 @@
+// https://www.codewars.com/kata/582e0e592029ea10530009ce/train/rust
+
 fn duck_duck_goose(players: &[Player], goose: u32) -> &'static str {
     let ln = players.len() as u32;
     let out = &players[((goose - 1) % ln) as usize];
     return out.name;
+}
+
+fn duck_duck_goose_BEST(players: &[Player], goose: u32) -> &'static str {
+    players[(goose - 1) as usize % players.len()].name
 }
