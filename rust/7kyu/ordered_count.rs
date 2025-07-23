d0418ccba09c8adccba3168415eada4fb2ee0f80
TheGoldKay <jhonnatanwfc@live.com>
2025-07-23 20:44:46 -0300

    no external package

--- rust/7kyu/ordered_count.rs
+++ rust/7kyu/ordered_count.rs
@@ -1,13 +1,14 @@
-use itertools::Itertools; 
+use std::collections::HashSet;
 use std::collections::HashMap;
 
 fn ordered_count(sip: &str) -> Vec<(char, i32)> {
     let mut count = HashMap::new();
     let mut ordered: Vec<(char, i32)> = Vec::new();
+    let mut seen: HashSet<char> = HashSet::new();
     for c in sip.chars() {
         *count.entry(c).or_insert(0) += 1;
     }
-    for c in sip.chars().unique() {
+    for c in sip.chars().filter(|c| seen.insert(*c)) {
         ordered.push((c, count[&c]));
     }
     ordered
