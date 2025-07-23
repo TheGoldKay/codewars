9efb7f0612950c352c1b33e01150c39d85c9b855
TheGoldKay <jhonnatanwfc@live.com>
2025-07-23 19:49:40 -0300

    less cluttered

--- rust/7kyu/ordered_count.rs
+++ rust/7kyu/ordered_count.rs
@@ -1,17 +1,14 @@
+use itertools::Itertools; 
 use std::collections::HashMap;
 
 fn ordered_count(sip: &str) -> Vec<(char, i32)> {
     let mut count = HashMap::new();
     let mut ordered: Vec<(char, i32)> = Vec::new();
-    let mut unique: Vec<char> = Vec::new();
     for c in sip.chars() {
         *count.entry(c).or_insert(0) += 1;
-        if !unique.contains(&c) {
-            unique.push(c);
-        }
     }
-    for c in unique.iter() {
-        ordered.push((*c, count[&c]));
+    for c in sip.chars().unique() {
+        ordered.push((c, count[&c]));
     }
     ordered
 }
