d3241a2fb8022e123e976336b53ff3b05a15f9d3
TheGoldKay <jhonnatanwfc@live.com>
2025-07-26 08:33:41 -0300

    improved version

--- rust/7kyu/sum_two_smallest_numbers.rs
+++ rust/7kyu/sum_two_smallest_numbers.rs
@@ -3,3 +3,17 @@ fn sum_two_smallest_numbers(numbers: &[u32]) -> u32 {
     arr.sort();
     arr[0] + arr[1]
 }
+
+fn sum_two_smallest_numbers(numbers: &[u32]) -> u32 {
+    let mut a: u32 = u32::MAX;
+    let mut b: u32 = u32::MAX;
+    for num in numbers.iter(){
+        if *num < a {
+            b = a;
+            a = *num;
+        }else if *num < b{
+            b = *num;
+        }
+    }
+    a + b
+}
