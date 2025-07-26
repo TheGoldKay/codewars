73079098433595237aff76303763375109e7cf7e
TheGoldKay <jhonnatanwfc@live.com>
2025-07-26 16:26:03 -0300

    better version

--- rust/7kyu/sort_by_length.rs
+++ rust/7kyu/sort_by_length.rs
@@ -3,3 +3,9 @@ fn sort_by_length(arr: &[String]) -> Vec<String> {
     v.sort_by(|a, b| a.len().cmp(&b.len()));
     v
 }
+
+fn sort_by_length_v2(arr: &[String]) -> Vec<String> {
+    let mut v: Vec<String> = arr.to_vec();
+    s.sort_by_key(String::len);
+    v
+}
